5b6ace052bafdbfca6770e945d83025f92dcf7b8
Drew Williams <drew@Jareds-MacBook-Air.local>
2024-06-27 22:35:01 -0500

    studio complete sans bonus

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -9,6 +9,26 @@
 // 5. Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
 
+function reverseCharacters(text) {
+    if(typeof(text) === "string"){
+        text = text.split("").reverse().join("")
+    } else if(typeof(text) === "number"){
+        text = String(text);
+        text = text.split("").reverse().join("");
+        text = Number(text);
+    }
+    return text;
+}
+
+
+let testString = "Andrew";
+let testNum = 1234;
+
+console.log(reverseCharacters(testString));
+console.log(reverseCharacters(testNum));
+
+
+
 // Part Two: Reverse Digits
 
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
@@ -16,6 +36,9 @@
 // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
 // 4. Return the reversed number.
 // 5. Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
+
+
+
 
 // Part Three: Complete Reversal
 
@@ -25,11 +48,16 @@
 // 4. Add the reversed string (or number) to the array defined in part ‘a’.
 // 5. Return the final, reversed array.
 // 6. Be sure to print the results from each test case in order to verify your code.
+let emptyArr = [];
 
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
 
+for(let i = 0; i < arrayTest3.length; i++){
+    emptyArr.unshift(reverseCharacters(arrayTest3[i]));
+}
+console.log(emptyArr);
 // Bonus Missions
 
 // 1. Have a clear, descriptive name like funPhrase.
