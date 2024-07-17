be53bbb7f3c76e36739349793a67727b344d09d6
Drew Williams <drew@Jareds-MacBook-Air.local>
2024-07-17 16:34:19 -0500

    exception exercises complete

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,14 @@
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
-    let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+
+  for (let i = 0; i < labs.length; i++) {
+    try {
+      let lab = labs[i];
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    }
+    catch (err) {
+      result = console.log("Error thrown")
+    }
   }
 }
 
@@ -10,15 +16,38 @@ let studentLabs = [
   {
     student: 'Carly',
     runLab: function (num) {
-        return Math.pow(num, num);
+      return Math.pow(num, num);
     }
   },
   {
     student: 'Erica',
     runLab: function (num) {
-        return num * num;
+      return num * num;
     }
   }
 ];
 
+let studentLabs2 = [
+  {
+    student: 'Blake',
+    myCode: function (num) {
+      return Math.pow(num, num);
+    }
+  },
+  {
+    student: 'Jessica',
+    runLab: function (num) {
+      return Math.pow(num, num);
+    }
+  },
+  {
+    student: 'Mya',
+    runLab: function (num) {
+      return num * num;
+    }
+  }
+];
+
+
 gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
