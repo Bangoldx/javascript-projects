093783a9adc55a6da707a1ba4aa7223fb2955937
Drew Williams <drew@Jareds-MacBook-Air.local>
2024-07-22 20:29:32 -0500

    classes studio complete

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,4 +1,59 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
+class CrewCandidate {
+    constructor(name, mass, scores) {
+        this.name = name;
+        this.mass = mass;
+        this.scores = scores;
+    }
+    addScore(score) {
+        this.scores.push(score);
+    }
+    average() {
+        let total = 0;
+        let average = 0
+        for (let i = 0; i < this.scores.length; i++) {
+            total += this.scores[i]
+        }
+        average = Math.round((total / this.scores.length) * 10) / 10;
+        return average
+    }
+    status() {
+        let average = this.average();
+
+        if (average >= 90) {
+            return "Accepted";
+        } else if (average >= 80 && average < 90) {
+            return "Reserved";
+        } else if (average >= 70 && average < 80) {
+            return "Probationary";
+        } else {
+            return "Rejected";
+        }
+    }
+}
+const bubbaBear = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
+const merryMaltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
+const gladGator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
+
+console.log(bubbaBear);
+console.log(merryMaltese);
+console.log(gladGator);
+
+bubbaBear.addScore(83);
+console.log(bubbaBear);
+
+console.log(merryMaltese.average());
+
+console.log(`${bubbaBear.name} earned an average test score of ${bubbaBear.average()}% and has a status of ${bubbaBear.status()}.`)
+console.log(`${merryMaltese.name} earned an average test score of ${merryMaltese.average()}% and has a status of ${merryMaltese.status()}.`)
+console.log(`${gladGator.name} earned an average test score of ${gladGator.average()}% and has a status of ${gladGator.status()}.`)
+
+let steps = 0;
+while(gladGator.average() <=80 ){
+    steps += 1;
+    gladGator.addScore(100);
+}
+console.log(steps);
 
 
 
