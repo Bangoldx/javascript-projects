4eea8d83c96210b8c942125fcb769f9da940f045
Drew Williams <drew@Jareds-MacBook-Air.local>
2024-08-05 16:40:28 -0500

    dom exercises complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,22 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+
+    button.addEventListener("click", function(){
+        paragraph.innerHTML = ("Houston, we have liftoff!")
+    })
+    missionAbort.addEventListener("mouseover", function(){
+        missionAbort.style.backgroundColor= "red";
+    })
+    missionAbort.addEventListener("mouseout", function(event){
+        event.target.style.backgroundColor= "";
+    })
+    missionAbort.addEventListener("click", function(){
+       let confirmation = window.confirm("Are you sure you want to abort the mission?")
+       if(confirmation === true){
+        paragraph.innerHTML = "Mission aborted! Space shuttle returning home"
+       }
+    })
 }
 
 window.addEventListener("load", init);
